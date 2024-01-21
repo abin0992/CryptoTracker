21c70d768baa315dc2076c714b7f36b128220759
Abin Baby <abin0992@gmail.com>
2024-01-21 19:08:02 +0100

    Initial Commit

---
+++ CryptoTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CryptoTracker
+//
+//  Created by Abin Baby on 21.01.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
